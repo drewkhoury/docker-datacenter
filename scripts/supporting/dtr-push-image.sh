a4a2042138b396e42bf0a15ce1faeb7315f56c9b
Andrew Khoury <drew.khoury@gmail.com>
2016-04-21 20:23:17 +1000

    dtr fixes

--- scripts/supporting/dtr-push-image.sh
+++ scripts/supporting/dtr-push-image.sh
@@ -8,7 +8,7 @@ curl -k -Lik \
 	--user admin:adminadmin -X POST \
 	--header "Content-Type: application/json" \
 	--header "Accept: application/json" \
-    -d "{  \"name\": \"foo\",  \"shortDescription\": \"foo\",  \"longDescription\": \"foo\",  \"visibility\": \"public\"}" "https://${DOMAIN}/api/v0/repositories/admin"
+    -d "{  \"name\": \"foo\",  \"shortDescription\": \"foo\",  \"longDescription\": \"foo\",  \"visibility\": \"public\"}" "https://${DOMAIN}:${DTR_PORT}/api/v0/repositories/admin"
 
 # login to dtr
 docker login -u admin -p adminadmin -e foo@bar.com ${DOMAIN}:${DTR_PORT}
@@ -23,9 +23,9 @@ EOF
 
 # create a image tag locally,
 # image name `${DOMAIN}/admin/foo` must match repo name `https://${DOMAIN}/repositories/admin/foo/details`
-docker build -t ${DOMAIN}/admin/foo:tag1 .
+docker build -t ${DOMAIN}:${DTR_PORT}/admin/foo:tag1 .
 
 # push the image tag to the dtr repo,
 # a repo can hold many image tags for the same image,
 # e.g `${DOMAIN}/admin/foo:tag1` and `${DOMAIN}/admin/foo:tag2`
-docker push ${DOMAIN}/admin/foo:tag1
+docker push ${DOMAIN}:${DTR_PORT}/admin/foo:tag1
